92d9c173eb478daf59169aa8f6c508bd8bb4cce1
irfan <irfanfadilla8513@gmail.com>
2023-10-03 11:37:54 +0700

    Add test suite and test case for teman diabetes

---
+++ Scripts/Add Gula Darah Sebelum Makan Siang/Script1696263515796.groovy
@@ -0,0 +1,55 @@
+import static com.kms.katalon.core.checkpoint.CheckpointFactory.findCheckpoint
+import static com.kms.katalon.core.testcase.TestCaseFactory.findTestCase
+import static com.kms.katalon.core.testdata.TestDataFactory.findTestData
+import static com.kms.katalon.core.testobject.ObjectRepository.findTestObject
+import static com.kms.katalon.core.testobject.ObjectRepository.findWindowsObject
+import com.kms.katalon.core.checkpoint.Checkpoint as Checkpoint
+import com.kms.katalon.core.cucumber.keyword.CucumberBuiltinKeywords as CucumberKW
+import com.kms.katalon.core.mobile.keyword.MobileBuiltInKeywords as Mobile
+import com.kms.katalon.core.model.FailureHandling as FailureHandling
+import com.kms.katalon.core.testcase.TestCase as TestCase
+import com.kms.katalon.core.testdata.TestData as TestData
+import com.kms.katalon.core.testng.keyword.TestNGBuiltinKeywords as TestNGKW
+import com.kms.katalon.core.testobject.TestObject as TestObject
+import com.kms.katalon.core.webservice.keyword.WSBuiltInKeywords as WS
+import com.kms.katalon.core.webui.keyword.WebUiBuiltInKeywords as WebUI
+import com.kms.katalon.core.windows.keyword.WindowsBuiltinKeywords as Windows
+import internal.GlobalVariable as GlobalVariable
+import org.openqa.selenium.Keys as Keys
+
+Mobile.startExistingApplication('com.temandiabetes.android', FailureHandling.STOP_ON_FAILURE)
+
+Mobile.tap(findTestObject('Dashboard/rekamanButton'), 0)
+
+Mobile.tap(findTestObject('RekamanPage/DateChangeButton'), 0)
+
+Mobile.tap(findTestObject('RekamanPage/newRekamanGulaDarahButton'), 0)
+
+Mobile.tap(findTestObject('RekamanPage/ManualInputButton'), 0)
+
+Mobile.tap(findTestObject('GulaDarahManualPage/JenisGulaDarahDropdown'), 0)
+
+Mobile.tap(findTestObject('GulaDarahManualPage/android.widget.CheckedTextView - Sebelum Makan Siang'), 0)
+
+Mobile.tap(findTestObject('GulaDarahManualPage/TimeInputDropdown'), 0)
+
+Mobile.tap(findTestObject('Dashboard/DateConfirmation'), 0)
+
+Mobile.sendKeys(findTestObject('GulaDarahManualPage/GulaDarahJumlahInput'), '123')
+
+Mobile.sendKeys(findTestObject('GulaDarahManualPage/SarapanSebelumnya'), 'Nasi')
+
+Mobile.scrollToText('Silahkan isi disini', FailureHandling.STOP_ON_FAILURE)
+
+Mobile.sendKeys(findTestObject('GulaDarahManualPage/SnackSebelumnya'), 'Wafer')
+
+Mobile.tap(findTestObject('RekamanPage/DoneConfirmation'), 0)
+
+Mobile.verifyElementText(findTestObject('GulaDarahManualPage/JumlahGulaDarahPopup'), '123 mg/dl')
+
+Mobile.verifyElementText(findTestObject('GulaDarahManualPage/SarapanPopup'), 'Nasi')
+
+Mobile.verifyElementText(findTestObject('GulaDarahManualPage/SnackPopup'), 'Wafer')
+
+Mobile.closeApplication()
+
---
+++ Test Suites/Add Manual Gula Darah.ts
@@ -0,0 +1,35 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>Add Manual Gula Darah</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>3</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>true</rerunImmediately>
+   <testSuiteGuid>d0ccf4e4-46c0-4f15-b343-dd2405ab076e</testSuiteGuid>
+   <testCaseLink>
+      <guid>a00f6fbb-9cf3-4eec-857d-46abbfea501d</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Add Gula Darah Lainnya</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>9319cc96-c97b-426f-9604-f1e0103eaa82</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Add Gula Darah 1-2 Jam Setelah Makan Siang</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>ff371077-7aa2-4785-a9b8-7c41d76df4ac</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Add Gula Darah Sebelum Makan Siang</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+</TestSuiteEntity>
